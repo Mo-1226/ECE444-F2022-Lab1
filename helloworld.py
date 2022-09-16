3c469913f7aa5cf955655a650e93f31adc5e416b
Mo-1226 <mohammadahmed2026@gmail.com>
2022-09-16 12:36:32 -0400

    commit one activity 5

--- helloworld.py
+++ helloworld.py
@@ -3,6 +3,11 @@ print("Hello World I have been at UofT for 4 years!")
 
 print("Hello World Mohammad Ahmed")
 
+<<<<<<< HEAD
 print("This is commit three")
+=======
+print("This is commit one")
+
+>>>>>>> 5465bed... commit one activity 5
 
 print("This is commit four")
